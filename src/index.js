399150350f826964132e0325bff0f98f1c98bff1
MoussaB06 <mohamedmoussa.benmouhoub@se.univ-bejaia.dz>
2025-07-07 17:49:28 +0100

    new changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-
+import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={5}
+        handleMovieRating={setMovieRating}
+      />
+      <p>this movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
